6326784544f7e3cb37ee51db9b5f1a1e98ff51fb
Kien Trinh <trongkien0402@gmail.com>
2026-01-12 11:42:39 -0800

    Initial Commit

---
+++ BlueProof/BlueProofApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlueProofApp.swift
+//  BlueProof
+//
+//  Created by Kien Trinh on 1/12/26.
+//
+
+import SwiftUI
+
+@main
+struct BlueProofApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
