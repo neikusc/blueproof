09799db971d72993b2a7f65c9ae4f4bc40f4a75c
Kien Trinh <trongkien0402@gmail.com>
2026-01-21 22:16:11 -0800

    Grammar and tone correction app

---
+++ BlueProof/KeychainService.swift
@@ -0,0 +1,61 @@
+import Foundation
+import Security
+
+enum KeychainService {
+    
+    private static let service = "com.kientrinh.BlueProof"
+    private static let account = "openai_api_key"
+    
+    static func saveApiKey(_ key: String) throws {
+        let data = key.data(using: .utf8)!
+        
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: account
+        ]
+        
+        // Remove existing key if any
+        SecItemDelete(query as CFDictionary)
+        
+        let attributes: [String: Any] = query.merging([
+            kSecValueData as String: data
+        ]) { $1 }
+        
+        let status = SecItemAdd(attributes as CFDictionary, nil)
+        guard status == errSecSuccess else {
+            throw NSError(domain: "Keychain", code: Int(status))
+        }
+    }
+    
+    static func loadApiKey() -> String? {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: account,
+            kSecReturnData as String: true,
+            kSecMatchLimit as String: kSecMatchLimitOne
+        ]
+        
+        var result: AnyObject?
+        let status = SecItemCopyMatching(query as CFDictionary, &result)
+        
+        guard status == errSecSuccess,
+              let data = result as? Data,
+              let key = String(data: data, encoding: .utf8) else {
+            return nil
+        }
+        
+        return key
+    }
+    
+    static func deleteApiKey() {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: account
+        ]
+        
+        SecItemDelete(query as CFDictionary)
+    }
+}
